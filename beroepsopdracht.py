d66ad274c7e165c636335eda60cc6409048acf94
Nabil <33580@ma-web.nl>
2021-10-08 13:08:00 +0200

    eerste commit

---
+++ beroepsopdracht.py
@@ -0,0 +1,27 @@
+while True:
+    print("Hello You!, Ik ben Noor Alattar");
+    print("Ik ben 22 jaar, en ik kom uit Irak")
+    print("wie ben jij?");
+    username = input("Enter username:");
+    print("hello: " + username + " ik zal jou mijn verhaal vertellen");
+    print("druk op A om te beginnen")
+    knopIngedrukt = input()
+    if knopIngedrukt == "a":
+        print("er is een groot gebrek aan vrijheid in mijn land Irak.")
+        print("niet iedereen kan zeggen wat hij of zij wilt, ik wil graag journalist worden")
+        print("om dat soort dingen aan het licht te brengen")
+        print("ik heb 700.000 volgers op social media, en ik ben bezig in het televisievak")
+        print("op een dag lijkt het erop dat ik mijn droom heb gekregen. ik mag werken voor een van de grootste tv zenders van het land")
+        print("wil je dit bod aannemen?")
+        print("A. ja")
+        print("B. nee ga verder met social media")
+        knopIngedrukt = input()
+    if knopIngedrukt == "a":
+        print("gefeliciteerd, je bent aangeomen als politiek verslaggever bij de grote zender")
+        print("je kunt nu eindelijk op grootschalige wijze problemen in het land aankaarten")
+        print("het gaat erg goed met je baan en je slaagt erin problemen als ongelijkheid aan het licht te brengen")
+        print("totdat je op een dag wordt benaderd door een geblindeerde bus.")
+        print("er komen mannen uit en je bent snel omsingeld. je word gedwongen in te stappen.")
+    elif knopIngedrukt == "b":
+        print("je hebt het aanbod afgewezen, en gaat door op social media")
+        print("je kunt nogsteeds mensen bereiken en je bericht overbrengen")
