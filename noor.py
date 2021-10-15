31e656b095cc616ed06094f763e407a43b914658
Nabil <33580@ma-web.nl>
2021-10-15 13:09:32 +0200

    progressie

---
+++ noor.py
@@ -0,0 +1,148 @@
+while True:
+    print("Hello You!, Ik ben Noor Alattar");
+    print("Ik ben 22 jaar, en ik kom uit Irak")
+    print("wie ben jij?");
+    username = input("Enter username:");
+    print("hello: " + username + " ik zal jou mijn verhaal vertellen");
+    print("druk op A om te beginnen")
+    knopIngedrukt = input()
+    if knopIngedrukt == "a":
+        print("er is een groot gebrek aan vrijheid in mijn land Irak.")
+        print("niet iedereen kan zeggen wat hij of zij wilt, ik wil graag journalist worden")
+        print("om dat soort dingen aan het licht te brengen")
+        print("ik heb 700.000 volgers op social media, en ik ben bezig in het televisievak")
+        print("op een dag lijkt het erop dat ik mijn droom heb gekregen. ik mag werken voor een van de grootste tv zenders van het land")
+        print("wil je dit bod aannemen?")
+        print("1. ja")
+        print("2. nee ga verder met social media")
+        knopIngedrukt = input()
+    if knopIngedrukt == "1":
+       
+       
+        #stuk 1 keuze 1
+        print("gefeliciteerd, je bent aangenomen als politiek verslaggever bij de grote zender")
+        print("je kunt nu eindelijk op grootschalige wijze problemen in het land aankaarten")
+        print("het gaat erg goed met je baan en je slaagt erin problemen als ongelijkheid aan het licht te brengen")
+        print("totdat je op een dag wordt benaderd door een geblindeerde bus.")
+        print("er komen mannen uit en je bent snel omsingeld. je word gedwongen in te stappen.")
+        print("wat doe je?")
+        print("1. rennen")
+        print("2. Meegaan")
+        knopIngedrukt = input()
+        if knopIngedrukt == "1":
+            
+            #stuk 2 keuze 1
+            print("je neemt een poging om te ontsnappen")
+            print("het is je net gelukt om te ontsnappen")
+            print("je beseft nu pas hoeveel mensen tegen jou zijn")
+            print("er komen zelfs dreigbrieven aan bij je huis")
+            print("in dit land blijven is op dit moment erg gevaarlijk")
+            print("gezien je erg bekend bent, is het ook moeilijk om over straat te lopen")
+            print("wat doe je? onderduiken of vluchten uit je land?")
+            knopIngedrukt = input()
+        if knopIngedrukt == "2":
+           
+            #stuk 2 keuze 2
+            print("je gaat mee")
+            print("je word naar een donkere kamer gebracht, en al je bezittingen worden afgepakt")
+            print("ze breken je hand, en je hoort dat als je nog een keer zulke onderwerpen aankaart")
+            print("je leven in groot gevaar zal zijn")
+            print("na een paar dagen word je vrijgelaten, maar verder gaan is gevaarlijk")
+            print("hier langer blijven kan erg nadelig voor je worden")
+            print("je besluit te vluchten uit Irak, en gaat naar europa")
+            print("wil ja via turkije of egypte gaan?")
+            print("1. Egypte")
+            print("2. Turkije")
+            knopIngedrukt = input()
+            if knopIngedrukt == "1":
+                print("je naar Egypte daarna zou je met de boot naar Griekenland kunnen")
+                print("aangezien je niks hebt moet je lopend naar egypte")
+            if knopIngedrukt == "2":
+                print("je gaat lopend naar Turkije, zo zou je aan kunnen komen")
+                print("in griekenland")
+                print("als je geluk hebt kan je in een vluchtelingenkamp terecht komen")
+                print("de omstandigheden zijn daar erg zwaar")
+                print("maar het is jouw enige optie op een succesvolle toekomst")
+                print("ookal is je hoogste prioriteit veiligheid, je wilt ook verder gaan met journalistiek")
+                print("maar eerst moet je")
+   
+        #stuk 1 keuze 2
+    elif knopIngedrukt == "2":
+        print("je hebt het aanbod afgewezen, en gaat door op social media")
+        print("je kunt nogsteeds mensen bereiken en je bericht overbrengen")
+        print("mensen vertellen je dat je erg gevaarlijk bezig bent")
+        print("erg komen steeds meer bedreigingen naar je toe via sociale media")
+        print("als je zo door gaat kan het erg gevaarlijk worden")
+        print("het is veiliger om te stoppen maar je wilt liever verder")
+        print("de enige optie is om naar een ander land te gaan")
+        print("je zou naar een land kunnen gaan met veel vrijheid")
+        print("naar welk land zou je willen gaan?")
+        print("je kunt kiezen uit, nederland, canada, en engeland")
+        print("1. Nederland")
+        print("2. Canada")
+        print("3. Engeland")
+        knopIngedrukt = input()
+        if knopIngedrukt == "1":
+            
+            
+            #stuk 2 keuze 1
+            print("je bent van plan om naar Nederland te gaan.")
+            print("een erg vrij land waar jouw onderwerpen mogelijk gehoord")
+            print("kunnen worden")
+            print("helaas gaat het niet zo makkelijk. je moet eerst toegelaten worden")
+            print("als je daar niet in slaagt zal je weer terug moeten")
+            print("over het algemeen is Nederland een land waar je makkelijk in zou")
+            print("kunnen komen")
+            print("je vraagt een visum aan en wacht een maand om te horen of je")
+            print("in Nederland mag.")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print("je hoort goed nieuws, je mag eindelijk naar nederland")
+            print("gefeliciteerd")
+        
+        elif knopIngedrukt == "2":
+           
+           
+            #stuk 2 keuze 2
+            print("Canada is een goede keuze gezien de vrijheid")
+            print("het kan wellicht lastig worden em er definitief te wonen")
+            print("de vlucht zal ook erg lang zijn,")
+            print("maar voor jouw huidige doeleindes is het geen verkeerde keuze")
+            print("al is de kans hier groter dat je niet word toegelaten")
+            print("als je wel toegelaten word, wacht jou een goed leven")
+            print("je wacht.")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print(".")
+            print("helaas, Canada wilt jou niet toelaten")
+            print("gezien alle andere landen nu ook geen plek hebben, ben je genoodzaakt een andere te kiezen")
+            print("je kunt naar Rusland, of Italië")
+            print("welke kies je?")
+            knopIngedrukt = input()
+            if knopIngedrukt == "1":
+                print("je gaat naar Rusland, daarna kun je naar een ander land")
+                print("in jouw aankomst in rusland boek je een hotelkamer")
+                print("door gebrek aan geld kun je niet lang blijven, en moet je binnen")
+                print("enkele dagen al naar een ander land")
+                print("")
+
+            
+            elif knopIngedrukt == "2":
+                print("je arriveert in Italië. je kan niet zo lang blijven dus")
+                print("moet je je tijd in Italië goed gebruiken om definitief in een ander land te kunnen verblijven")
+                print("")
+
+        
+        
+        
+
+
+       
+        
